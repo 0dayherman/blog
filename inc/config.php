82f4683d710a7881cea6d489381946c80e93b1cd
apaajalahx <52324987+apaajalahx@users.noreply.github.com>
2019-09-07 22:06:27 +0700

    Add files via upload

---
+++ inc/config.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once "db.php";
+
+$host		= "localhost";
+
+$username	= "root";
+
+$password	= "";
+
+$database	= "dbtest";
+
+
+$db = new DB($host, $username, $password, $database);
+
---
+++ inc/db.php
@@ -0,0 +1,67 @@
+<?php
+
+class DB{
+
+/**
+ *	@author Dinar Hamid
+ *  @version 1.0
+ *	@license MIT
+ *	https://opensource.org/licenses/MIT
+ */
+
+	public $host;
+
+	public $user;
+
+	public $password;
+
+	public $dbname;
+
+	public $db;
+
+	public function __construct($host,$user,$password,$dbname){
+
+		$this->host = $host;
+		$this->user = $user;
+		$this->password = $password;
+		$this->dbname = $dbname;
+		$this->db = new mysqli($this->host,$this->user,$this->password,$this->dbname);
+
+ 	}
+
+	public function artikel($id){
+
+		$prepare = $this->db->prepare("SELECT * FROM news WHERE id = ?");
+
+		$prepare->bind_param("s", $id);
+
+		$prepare->execute();
+
+		$result = $prepare->get_result();
+
+		while($tampil = $result->fetch_row()){
+			return $tampil;
+		}
+		$prepare->close();
+		$this->db->close();
+
+  }
+
+  public function cari($search){
+
+  		$prepare = $this->db->prepare("SELECT id,title,isi FROM news WHERE title LIKE ?");
+
+  		$prepare->bind_param("s", $search);
+
+  		$prepare->execute();
+
+  		$prepare->bin_result($title, $isi);
+
+  		while($prepare->fetch()){
+  			return $title."<br>".$isi;
+  		}
+  		$prepare->close();
+  		$this->db->close();
+  }
+
+}
