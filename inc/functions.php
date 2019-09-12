e685edde9c94dcba5f5baf02892ef756535471fb
apaajalahx <52324987+apaajalahx@users.noreply.github.com>
2019-09-12 15:25:14 +0700

    Create functions.php

---
+++ inc/functions.php
@@ -0,0 +1,38 @@
+<?php
+
+
+/**
+ * same as wordpress, i love wordpress
+ */
+function connect_to_db(){
+	global $db;
+
+	require_once "db.php";
+	require_once __DIR__. "/../config.php";
+
+	if(isset($db)) {
+		return;
+	}
+
+	$host 	  =	defined( 'HOST' ) ? HOST : '';
+	$username =	defined( 'USERNAME' ) ? USERNAME : '';
+	$password = defined( 'PASSWORD' ) ? PASSWORD : '';
+	$database = defined( 'DATABASE' ) ? DATABASE : '';
+
+	$db = new DB($host,$username,$password,$database);
+
+}
+
+function check_installed(){
+
+	if(!is_file(__DIR__ . "/../config.php")){
+		echo "This Not Installed Properly";
+	}
+}
+
+function error($er){
+
+		echo '<link rel="stylesheet" href="theme/css/error.css">';
+		echo '<p class="error">'.($er).'</p>';
+		
+}
