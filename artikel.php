60264dae9ab33dfb78949347ff170aa06ba2e854
apaajalahx <52324987+apaajalahx@users.noreply.github.com>
2019-09-12 15:41:21 +0700

    Update artikel.php

--- artikel.php
+++ artikel.php
@@ -11,7 +11,20 @@ echo "<br><br><br>";
 if(@$_GET['id']){
 $id = @$_GET['id'];
 $tampil = $db->articles($id);
-	echo $tampil['1'];
+	echo '<section class="artikel" id="artikel">
+  <div class="container-artikel">
+    <div class="row">
+      <div class="col text-justify mt-5">
+        <h3>'.$tampil['1'].'</h3>
+        <p>
+        Author by <i class="fa fa-user"></i> '.$tampil['4'].'<br>
+        Date post '.$tampil['3'].'
+        </p>
+        <p>'.$tampil['2'].'.</p>
+      </div>
+    </div>
+  </div>
+</section>';
 } else {
 	# echo $db->thumb_artikel();
 }
