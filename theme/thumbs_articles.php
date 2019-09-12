b28ab1d857ef7d8fa73063aaf5fa61df42947ce2
apaajalahx <52324987+apaajalahx@users.noreply.github.com>
2019-09-12 17:55:49 +0700

    Add files via upload

---
+++ theme/thumbs_articles.php
@@ -0,0 +1,16 @@
+<?php while($pecah=$result->fetch_row()) { ?>
+    <div class="col-md">
+      <div class="card">
+        <img src="img/thumbs/1.png" class="card-img-top" alt="Card image cap">
+        <div class="card-body">
+          <span id="admin"><?php echo $pecah['4'];?></span>
+          <span id="date"><?php echo $pecah['3'];?></span>
+          <i class="fa fa-comment-o" id="comment"> 2</i>
+          <h5 class="card-title"><?php echo $pecah['1'];?></h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="articles/<?php echo $pecah['0'];?>" class="btn btn-primary">Read more</a>
+      </div>
+     </div>
+    </div>
+<?php
+}
