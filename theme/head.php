d7fb16a1a1dcb4abb64a3102d33100e2aaa8c588
Hermansyah <43913036+0dayherman@users.noreply.github.com>
2019-09-12 16:52:31 +0700

    Update head.php

--- theme/head.php
+++ theme/head.php
@@ -1,90 +1,34 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Dawning+of+a+New+Day" rel="stylesheet">
+require_once "load.php";
+include __DIR__ . "/theme/head.php";
 
-
-    <title>W3lcome!</title>
-
-    <link rel="icon" type="image/x-icon" href="https://digitalent.kominfo.go.id/favicon.ico"/>
-  <style type="text/css">
-    label:after {
-    content: "_";
-    opacity: 0;
-    animation: cursor 1s infinite
-}
-
-@keyframes cursor {
-    0%,
-    100%,
-    40% {
-        opacity: 0
-    }
-    50%,
-    90% {
-        opacity: 1
-    }
-}
-#admin{
-  font-weight: bold;
-  font-size: 13px;
-}
-#date{
-  font-size: 13px;
-}
-#comment{
-  float: right;
-  font-size: 13px;
-}
-.container-artikel{
-  width: 700px;
-  margin-left: 8rem;
-}
-.card{
-  margin-left: 6rem;
-  margin-bottom: 10px;
-}
-  </style>
-  </head>
-  <body class="mt-5">
-  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="./"><img class="img-fluid" width="50px;" src="https://digitalent.kominfo.go.id/apple-touch-icon.png"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="./">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
-            </li>
-      <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-              Artikel
-            </a>
-            <div class="dropdown-menu">
-              <a class="dropdown-item" href="#">HTML</a>
-              <a class="dropdown-item" href="#">CSS</a>
-              <a class="dropdown-item" href="#">PHP</a>
-              <a class="dropdown-item" href="#">MYSQL</a>
-              <a class="dropdown-item" href="#">JAVASCRIPT</a>
-              <a class="dropdown-item" href="#">JQUERY</a>
-              </div>
-            </li>
-          </ul>
+if(isset($_POST['cari'])){
+$search = @"%{$_POST['cari']}%";
+$tampil = $db->cari($search);
+?>
+<section id="artikel" class="artikel bg-light">
+<div class="container">
+  <div class="row mb-3 pt-5 ml-1">
+    <h3><?php echo "You Search : ".htmlspecialchars($_POST['cari']);?></h3>
+</div>
+</div>
+    <div class="col">
+   <div class="col-md">
+      <div class="card" style="width: 18rem; margin-left: 6rem;">
+        <!-- <img src="img/thumbs/3.png" class="card-img-top" alt="Card image cap"> -->
+        <div class="card-body">
+          <span id="admin"><?php echo $tampil['4'];?></span>
+          <span id="date"><?php echo $tampil['3'];?></span>
+          <!-- <i class="fa fa-comment-o" id="comment"> 2</i> -->
+          <h5 class="card-title"><?php echo $tampil['1'];?></h5>
+          <p class="card-text text-justify"><?php echo substr($tampil['2'],0,100);?>..</p>
+          <a href="<?php echo 'artikel.php?id='.$tampil['0'];?>" class="btn btn-primary">Read more</a>
         </div>
-        <form method="POST" action="search.php" class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" name="cari" placeholder="Cari.." aria-label="Search">
-      <button class="btn btn-outline-primary" type="submit">Cari</button>
-    </form>
-      </nav>
+      </div>
+    </div>
+  </div>
+</section>
+<?php
+}
+include "theme/footer.php";
