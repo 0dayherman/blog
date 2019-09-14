d5997482ac51fc32173287ec9e065246b93db508
apaajalahx <52324987+apaajalahx@users.noreply.github.com>
2019-09-14 20:41:42 +0700

    Create 404.php

---
+++ theme/404.php
@@ -0,0 +1,19 @@
+<?php require_once __DIR__ . "/../load.php"; ?>
+
+<!DOCTYPE html>
+	 <html>
+	   <head>
+	   	<title>404 Not Found</title>
+	   	<!-- Responsive -->
+	   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <style>
+<?php include __DIR__ . '/css/min.css';?>
+
+		</style>
+	   </head>
+          	<div class="notfound">
+         	Page 404 Not Found!
+         	<br>
+         	<a href="<?php echo $root;?>">Back To Home!</a>
+         </div>
+   </html>
